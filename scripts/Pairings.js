7c5245fd467fe5ef754ab200b97cb2c0382e530c
Jonathan Castro <jonathancastro55@gmail.com>
2024-09-29 14:15:12 -0500

    fixed Pairnigs module

--- scripts/Pairings.js
+++ scripts/Pairings.js
@@ -24,7 +24,7 @@ export const Pairings = () => {
         const kidsStar = findCelebrityMatch(kid, celebrities)
         html += `
             <li>
-                ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.Sport} star, by ${kid.wish}
+                ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.sport} star, by ${kid.wish}
             </li>
         `
     }
